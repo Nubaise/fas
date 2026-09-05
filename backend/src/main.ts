3fafad60017474047287939f61cad672a4672ad1
Nubaise <113296164+Nubaise@users.noreply.github.com>
2026-09-06 01:31:22 +0530

    feat: enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  
   app.setGlobalPrefix('api');
 
   app.enableVersioning({
